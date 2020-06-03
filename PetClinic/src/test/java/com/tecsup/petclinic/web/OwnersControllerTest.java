6f12201d18826a28893e8b89d5d03143e3948578
JonathanHuarca <jonathan.huarca@tecsup.edu.pe>
2020-06-02 21:14:09 -0500

    Cambios realizados para jenkins

--- PetClinic/src/test/java/com/tecsup/petclinic/web/OwnersControllerTest.java
+++ PetClinic/src/test/java/com/tecsup/petclinic/web/OwnersControllerTest.java
@@ -62,7 +62,7 @@ public class OwnersControllerTest {
 	public void testFindOnwersOK() throws Exception {
 
 
-		String OWNERS_FIRSTNAME = "Camilo";
+		String OWNERS_FIRSTNAME = "George";
 	    String  LAST_NAME = "Franklin";
 		String  ADDRESS = "110 W. Liberty St.";
 		String  CITY = "Madison";
